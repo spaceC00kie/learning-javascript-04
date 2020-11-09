d370492069dd477de555ff83e738ab0517debc34
spaceC00kie <kirstie317@gmail.com>
2020-11-09 09:53:41 -0500

    accessing form elements

---
+++ accessingformelements.js
@@ -0,0 +1,5 @@
+<form name="kitty'sForm">
+    Username: <input type="text" name="username"/>
+    Password: <input type="password" name="password"/>
+    <input type="submit" value="Submit">
+</form>
